9fd1647e5e90663b5bfccc3fd2ecc640e353fddd
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-24 12:16:09 -0400

    HR-1: Initial
    HR: Initial
    
    HR: Adding images
    
    HR: travis update
    
    HR: moving gravisPre.sh
    
    HR: travis python
    
    HR: xcodeTest.sh
    
    HR: travis
    
    HR-1: travis update
    
    HR-1: Update podfile
    
    HR-1: podfile path
    
    HR-1: no travisPre
    
    HR-1: added heart.xcodeproj/xcshareddata/
    
    HR-1: no travis pre
    
    HR-1: remove pod from testing
    
    Dust
    
    HR-1: updated schema
    
    test
    
    Adding frameworks
    
    HR-1: added import
    
    HR-1: Code signing taken out
    
    HR-1: update readme

---
+++ go/visitor/src/house/visitor/visitor.go
@@ -0,0 +1,77 @@
+package visitor
+
+//Thanks to http://ecs.victoria.ac.nz/foswiki/pub/Main/TechnicalReportSeries/ECSTR11-01.pdf
+/*
+Ref:
+https://gist.github.com/francoishill/f0624e7760aacdc96b42
+*/
+import (
+	"fmt"
+)
+
+// We will have multiple car parts
+type CarPart interface {
+	Accept(CarPartVisitor)
+}
+
+type Wheel struct {
+	Name string
+}
+
+func (this *Wheel) Accept(visitor CarPartVisitor) {
+	visitor.visitWheel(this)
+}
+
+type Engine struct{}
+
+func (this *Engine) Accept(visitor CarPartVisitor) {
+	visitor.visitEngine(this)
+}
+
+type Car struct {
+	parts []CarPart
+}
+
+func NewCar() *Car {
+	this := new(Car)
+	this.parts = []CarPart{
+		&Wheel{"front left"},
+		&Wheel{"front right"},
+		&Wheel{"rear right"},
+		&Wheel{"rear left"},
+		&Engine{}}
+	return this
+}
+
+func (this *Car) Accept(visitor CarPartVisitor) {
+	for _, part := range this.parts {
+		part.Accept(visitor)
+	}
+}
+
+//Interface of the visitor
+type CarPartVisitor interface {
+	visitWheel(wheel *Wheel)
+	visitEngine(engine *Engine)
+}
+
+// Concrete Implementation of the visitor
+type GetMessageVisitor struct {
+	Messages []string
+}
+
+func (this *GetMessageVisitor) visitWheel(wheel *Wheel) {
+	this.Messages = append(this.Messages, fmt.Sprintf("Visiting the %v wheel\n", wheel.Name))
+}
+
+func (this *GetMessageVisitor) visitEngine(engine *Engine) {
+	this.Messages = append(this.Messages, fmt.Sprintf("Visiting engine\n"))
+}
+
+//Usage of the visitor
+func main() {
+	car := NewCar()
+	visitor := new(GetMessageVisitor)
+	car.Accept(visitor)
+	fmt.Println(visitor.Messages)
+}
