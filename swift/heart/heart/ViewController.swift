64eba9b939914bddc36caff17f3886276b56cb11
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-24 21:51:55 -0400

    HR-2: Google SignIn

--- swift/heart/heart/ViewController.swift
+++ swift/heart/heart/ViewController.swift
@@ -1,25 +1,34 @@
-//
-//  ViewController.swift
-//  heart
-//
-//  Created by Michael Chirico on 6/24/18.
-//  Copyright © 2018 Michael Chirico. All rights reserved.
-//
-
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+import GoogleSignIn
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,GIDSignInUIDelegate  {
+  var ref: DatabaseReference!
+  var db: Firestore!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    GIDSignIn.sharedInstance().uiDelegate = self
+    GIDSignIn.sharedInstance().signIn()
+    ref = Database.database().reference()
+    db = Firestore.firestore()
+    
+    //valid_accounts
+    self.ref.child("valid_accounts").child("d7AwlBFf6jcZCACrPsUfut1gg0f2").setValue(["username": "mchirico@gmail.com"])
+    
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
-
