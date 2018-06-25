31d482cd241cf2dd1209bbf414b2fba7a281f143
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-24 23:11:21 -0400

    Quiet

--- xcodeTest.sh
+++ xcodeTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd swift/heart/
-#xcodebuild clean build -workspace heart.xcworkspace -scheme heart CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4'
-xcodebuild test -workspace heart.xcworkspace CODE_SIGNING_REQUIRED=NO -scheme heart -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4'  -enableCodeCoverage  YES
+xcodebuild clean build -workspace heart.xcworkspace -scheme heart CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4' -quiet
+xcodebuild test -workspace heart.xcworkspace -scheme heart -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4'  -enableCodeCoverage  YES -quiet
