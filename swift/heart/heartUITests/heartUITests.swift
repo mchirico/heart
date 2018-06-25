9a53d4cc351ad305ae5ca1233b7bc36187692b67
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-24 22:33:37 -0400

    Comment

--- swift/heart/heartUITests/heartUITests.swift
+++ swift/heart/heartUITests/heartUITests.swift
@@ -6,23 +6,24 @@
 //  Copyright © 2018 Michael Chirico. All rights reserved.
 //
 
+
 import XCTest
 import UIKit
 import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+import GoogleSignIn
 
 class heartUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
     }
     
     override func tearDown() {
@@ -36,3 +37,4 @@ class heartUITests: XCTestCase {
     }
     
 }
+
